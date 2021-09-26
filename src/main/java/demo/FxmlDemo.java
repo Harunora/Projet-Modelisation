0dfa4d1ed4ebe81314d790162efe1341f7e86c95
Beelzashi <Beelzashi@users.noreply.github.com>
2021-09-26 12:44:08 +0200

    Add FXML main view

--- src/main/java/demo/FxmlDemo.java
+++ src/main/java/demo/FxmlDemo.java
@@ -13,7 +13,7 @@ public class FxmlDemo extends Application {
 	// java --module-path 'path-to-javafx-binaries/lib' --add-modules javafx.controls,javafx.fxml -jar .\projetmode.jar
 	public void start(Stage stage) throws IOException {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/simpleFxmlDemo.fxml"));
+            loader.setLocation(getClass().getResource("/MainView.fxml"));
             Parent root = loader.load();
 
             Scene scene = new Scene(root);
