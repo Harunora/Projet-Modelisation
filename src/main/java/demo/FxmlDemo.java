2d0d898256b57b707f1bd037edfbe461dd96af14
Beelzashi <Beelzashi@users.noreply.github.com>
2021-09-28 21:58:33 +0200

    ExplorerFile is runable

--- src/main/java/demo/FxmlDemo.java
+++ src/main/java/demo/FxmlDemo.java
@@ -10,17 +10,23 @@ import javafx.fxml.FXMLLoader;
 
 public class FxmlDemo extends Application {
 
-	// java --module-path 'path-to-javafx-binaries/lib' --add-modules javafx.controls,javafx.fxml -jar .\projetmode.jar
-	public void start(Stage stage) throws IOException {
+    // java --module-path 'path-to-javafx-binaries/lib' --add-modules javafx.controls,javafx.fxml -jar .\projetmode.jar
+    public void start(Stage stage) throws IOException {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/MainView.fxml"));
             Parent root = loader.load();
-
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setTitle("FXML demo");
             stage.show();
+            
+            
+        
     }
+    public Stage getStage() {
+    	return getStage();
+    }
+    
 
     public static void main(String[] args) {
             Application.launch(args);
--- src/main/java/demo/MainControler.java
+++ src/main/java/demo/MainControler.java
@@ -1,58 +1,55 @@
 package demo;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-public class MainControler extends Application implements Initializable {
-	@FXML
-	Button LoadFolder, bVue1, bVue2, bVue3, bVue4;
-	@FXML
-	TreeView<String> TV;
-	@FXML
-	Canvas canvas;
+public class MainControler implements Initializable {
+    @FXML
+    Button loadfolder, bVue1, bVue2, bVue3, bVue4;
+    
+    @FXML
+    TreeView<String> TV;
+    
+    @FXML
+    Canvas canvas;
 
-	public void start(Stage stage) throws IOException {
-		ExplorerFile  ef = new ExplorerFile(); 
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(getClass().getResource("/MainView.fxml"));
-		Parent root = loader.load();
-
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
-		stage.setTitle("3D View");
-		stage.show();
-		LoadFolder.setOnAction(new EventHandler<ActionEvent>() {
+    
+    public Stage stage = new Stage();
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    	
+        loadfolder.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				
-				TV =ExplorerFile.FolderSearcher(stage);
+				DirectoryChooser dc = new DirectoryChooser();
+				dc.setInitialDirectory(new File(System.getProperty("user.home")));
+				File choice = dc.showDialog(stage);
+				if (choice == null || !choice.isDirectory()) {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setHeaderText("Could not open directory");
+					alert.setContentText("The file is invalid.");
+
+					alert.showAndWait();
+				} else {
+					TV.setRoot(ExplorerFile.getNodesForDirectory(choice));
+				}
 			}
 		});
-	}
-
-	public static void main(String[] args) {
-		Application.launch(args);
-	}
-
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-	}
+        
+    }
+    
 
 }
