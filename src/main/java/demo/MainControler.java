5e805e8ed204f96441f3348df269df1477988254
Christopher Pottier <christopher.pottier.etu@univ-lille.fr>
2021-10-23 17:09:04 +0200

    Affichage casi fonctionnel

--- src/main/java/demo/MainControler.java
+++ src/main/java/demo/MainControler.java
@@ -14,6 +14,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.DirectoryChooser;
--- src/main/java/graph/CanvasWriter.java
+++ src/main/java/graph/CanvasWriter.java
@@ -3,42 +3,55 @@ package graph;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 
 public class CanvasWriter {
 	
-	
+	Canvas canvas;
 	GraphicsContext graphicContext;
 	Graph listface;
+	List<Color> color;
 	List<double[]> x;
 	List<double[]> y;
-	public CanvasWriter(GraphicsContext gc, Graph lf) {
+	
+	public CanvasWriter(Canvas c, Graph lf) {
+		
 		// TODO Auto-generated constructor stub
 		this.x=new ArrayList<double[]>();
 		this.y=new ArrayList<double[]>();
-		graphicContext=gc;
-		listface=lf;
-		for(int i=0;i<listface.getNbFaces();i++) {
-			double[] i1=new double[lf.faces.get(i).nbSommet];
-			double[] i2=new double[lf.faces.get(i).nbSommet];
-			for(int j=0;j<lf.faces.get(i).nbSommet;j++) {
-				i1[j]=lf.faces.get(i).sommets.get(j).getX();
-				i2[j]=lf.faces.get(i).sommets.get(j).getY();
-			}
-			this.x.add(i1);
-			this.y.add(i2);
-		}
+		this.canvas=c;
+		graphicContext=c.getGraphicsContext2D();
 		
-		writeOnCanvas();
+		listface=lf;
+		useGraph();
 	}
 	
 	public void writeOnCanvas() {
-		for(int i=0;i<x.get(i).length;i++) {
+		for(int i=0;i<x.size();i++) {
 			double[] i1=this.x.get(i);
 			double[] i2=this.y.get(i);
-			if(i1.length==i2.length) {
-			graphicContext.strokePolygon(i1,i2,i1.length);
-			}
+			
+			graphicContext.strokePolygon(i1, i2, i1.length);
+			graphicContext.setFill(null);
+			graphicContext.fillPolygon(i1, i2,i1.length);
 		}
+	}
+	
+	public void useGraph() {
+		for(int i=0;i<listface.getNbFaces();i++) {
+			double[] i1=new double[this.listface.faces.get(i).nbSommet];
+			double[] i2=new double[this.listface.faces.get(i).nbSommet];
+			for(int j=0;j<this.listface.faces.get(i).nbSommet;j++) {
+				i1[j]=this.listface.faces.get(i).sommets.get(j).getX()+canvas.getHeight()/2;
+				i2[j]=this.listface.faces.get(i).sommets.get(j).getY()+canvas.getWidth()/2;
+				System.out.println(i1[j]+";"+i2[j]);
+			}
+			
+			this.x.add(i1);
+			this.y.add(i2);
+			
+		}
+		writeOnCanvas();
 	}
 }
